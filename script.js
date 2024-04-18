7d3d91cdcc790185457f2858b15af800bb3ba9d0
deeprathod97 <deeprathod9797@gmail.com>
2024-04-18 20:48:06 +0530

    const, let and var in JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("JavaScript  #3: var, let and const")
+// var a = 45;
+// var a = "p"
+let b = "deep";
+const author = "deep"
+author = 5 // Throws an error because constant cannot be changed
+b = 4
+const deep = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
